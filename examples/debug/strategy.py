8f5fbc8f7543b53b21f069b11d8e17f9b4558c9a
luyh <yanhonglu5@icloud.com>
2019-10-08 12:00:32 +0800

    stratage debug

--- examples/debug/strategy.py
+++ examples/debug/strategy.py
@@ -1,25 +1,34 @@
+import ccxt
+
+from tensorforce.agents import Agent
+from tensorforce.environments import Environment
+
+from tensortrade.environments import TradingEnvironment
 from tensortrade.exchanges.simulated import FBMExchange
 from tensortrade.features.scalers import MinMaxNormalizer
 from tensortrade.features.stationarity import FractionalDifference
 from tensortrade.features import FeaturePipeline
 from tensortrade.rewards import SimpleProfitStrategy
 from tensortrade.actions import DiscreteActionStrategy
-from tensortrade.environments import TradingEnvironment
-from tensortrade.strategies.tensorforce_trading_strategy import TensorforceTradingStrategy
+from tensortrade.strategies import TensorforceTradingStrategy
 
-normalize_price = MinMaxNormalizer(["open", "high", "low", "close"])
-difference = FractionalDifference(difference_order=0.6)
-feature_pipeline = FeaturePipeline(normalize_price, difference)
+normalize = MinMaxNormalizer(inplace=True)
+difference = FractionalDifference(difference_order=0.6,
+                                  inplace=True,
+                                  all_column_names=["open", "high", "low", "close", "volume"])
+feature_pipeline = FeaturePipeline(steps=[normalize, difference])
+
+reward_strategy = SimpleProfitStrategy()
+action_strategy = DiscreteActionStrategy(n_actions=20, instrument_symbol='ETH/BTC')
+
 exchange = FBMExchange(base_instrument='BTC',
                        timeframe='1h',
-                       feature_pipeline=feature_pipeline)
-reward_strategy = SimpleProfitStrategy()
-action_strategy = DiscreteActionStrategy(n_actions=20,
-                                        instrument_symbol='ETH/BTC')
+                       should_pretransform_obs=True)
+
 environment = TradingEnvironment(exchange=exchange,
-                                 feature_pipeline=feature_pipeline,
                                  action_strategy=action_strategy,
-                                 reward_strategy=reward_strategy)
+                                 reward_strategy=reward_strategy,
+                                 feature_pipeline=feature_pipeline)
 
 agent_spec = {
     "type": "ppo_agent",
@@ -30,6 +39,7 @@ agent_spec = {
     "discount": 0.99,
     "likelihood_ratio_clipping": 0.2,
 }
+
 network_spec = [
     dict(type='dense', size=64, activation="tanh"),
     dict(type='dense', size=32, activation="tanh")
@@ -38,13 +48,15 @@ network_spec = [
 strategy = TensorforceTradingStrategy(environment=environment,
                                       agent_spec=agent_spec,
                                       network_spec=network_spec)
+
 # Callback function printing episode statistics
 def episode_finished(r):
     print( "Finished episode {ep} after {ts} timesteps (reward: {reward})".format( ep=r.episode, ts=r.episode_timestep,
                                                                                    reward=r.episode_rewards[-1] ) )
 
-performance = strategy.run(steps=100000,
-                           should_train=True,
+performance = strategy.run(episodes=2,
+                           testing=True,
                            episode_callback=episode_finished)
 
+print(performance[-5:])
 print('done')
