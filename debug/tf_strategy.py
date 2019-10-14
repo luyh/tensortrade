20cdcdb6e12264260e37517fa298638d76a25b8e
luyh <yanhonglu5@icloud.com>
2019-10-14 17:58:51 +0800

    分离debug

--- debug/tf_strategy.py
+++ debug/tf_strategy.py
@@ -23,7 +23,7 @@ action_strategy = DiscreteActionStrategy(n_actions=20, instrument_symbol='ETH/BT
 
 exchange = FBMExchange(base_instrument='BTC',
                        timeframe='1h',
-                       times_to_generate = int(1e6),
+                       times_to_generate = int(1e5),
                        should_pretransform_obs=True,
                        feature_pipeline=feature_pipeline)
 
